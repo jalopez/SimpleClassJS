535a635a7db9dbb42a04d79ea05c848683db26bd
Javier Lopez <jlopez@fi.upm.es>
2011-11-02 20:21:29 +0100

    With constructor

--- src/SimpleClass.js
+++ src/SimpleClass.js
@@ -1,6 +1,14 @@
 var Class = function(methods) {
-    var klass = function() {};
+    var constructor = function(){};
+    if (methods) {
+        if (methods.constructor && methods.constructor instanceof Function) {
+            constructor = methods.constructor;
+        }
+    }
 
+    var klass = function() {
+        return new constructor();
+    };
 
     return klass;
 };
--- test/spec/SpecSimpleClass.js
+++ test/spec/SpecSimpleClass.js
@@ -2,7 +2,22 @@ describe("SimpleClassJS", function() {
 
     it("should create an empty class", function() {
         var Empty = Class();    
-
         expect(Empty).toBeDefined();
+    });
+
+
+    it("should create a class with a constructor", function() {
+        var i = 0;
+        
+        var Simple = Class({
+            constructor: function() {
+                i++; 
+            }
+        });
+
+        var instance1 = new Simple(),
+            instance2 = new Simple();
+
+        expect(i).toBe(2);
     });
 });
